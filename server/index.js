ea143ade6b33f7bfa42f359c9016dd80e26a9957
sanfredy <fredy2137@gmail.com>
2025-07-17 11:29:26 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = 5000;
+// ¡CORREGIDO! Usa process.env.PORT para que Vercel asigne el puerto dinámicamente
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
